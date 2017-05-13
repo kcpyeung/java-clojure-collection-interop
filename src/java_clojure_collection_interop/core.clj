18b778c21165007caceec9119fb4e3411eaa1c4c
Kevin Yeung <github@yeung.online>
2017-05-13 12:29:50 +0800

    Rewrite map processing without filtering

--- src/java_clojure_collection_interop/core.clj
+++ src/java_clojure_collection_interop/core.clj
@@ -1,25 +1,18 @@
 (ns java-clojure-collection-interop.core)
 
-(defn- to-clojure-map [m]
-  (->> m
-    keys
-    (map (fn [k] [(keyword k) (.get m k)]))
-    (into {})))
-
-(defn- entries-with-java-map-values [m]
-  (->> m
-    seq
-    (filter #(instance? java.util.AbstractMap (second %)))))
-
-(defn- process-map [java-map]
-  "Converts Java Map to Clojure Associative"
-  (let [clj-map (to-clojure-map java-map)
-        map-value-entries (entries-with-java-map-values clj-map)]
-    (if (empty? map-value-entries)
-      clj-map
-      (->> map-value-entries
-        (map (fn [kv] [(first kv) (process-map (second kv))]))
-        (into clj-map)))))
+(defn process-map [java-map]
+  (letfn [(process-map-item [kv]
+            (if (instance? java.util.AbstractMap (second kv))
+              [(first kv) (process-map (second kv))]
+              kv))
+          (to-kvs [java-map]
+            (->> java-map
+              keys
+              (map (fn [k] [(keyword k) (.get java-map k)]))))]
+    (->> java-map
+      to-kvs
+      (map process-map-item)
+      (into {}))))
 
 (defn- process-list [java-list]
   (letfn [(process-list-item [item]
